c76f6464c0dea681a05ba3d93a7b0741126b19be
mercesolis <mersolis@live.com>
2020-10-24 23:47:04 -0500

    host stuff

--- FamApp/FamApp2/Startup.cs
+++ FamApp/FamApp2/Startup.cs
@@ -59,7 +59,7 @@ namespace FamApp2
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
             app.UseHttpsRedirection();
